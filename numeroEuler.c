d8b39abe77bc4a703fd7883d4bcc442428b02c49
Caique Costa <ccostafrias@gmail.com>
2025-05-31 13:15:22 -0300

    feat: enhance euler's number precision

--- numeroEuler.c
+++ numeroEuler.c
@@ -1,35 +1,25 @@
 #include <stdio.h>
 #include <locale.h>
 
-double fatorial(int n) {
-	if (n == 0) return 1;
-	int fat = n;
-	for (int i = n - 1; i > 1; i--) {
-		fat *= i;
-	}
-
-	return fat;
-}
-
 int main() {
     setlocale(LC_ALL, "Portuguese_Brazil");
 
-	int depth;
-	while (1) {
-		printf("Escolha a precisão: ");
-		scanf("%d", &depth);
+	int depth = 35;
+	// while (1) {
+	// 	printf("Escolha a precisão: ");
+	// 	scanf("%d", &depth);
 
-		if (depth > 0) break;
-	}
+	// 	if (depth > 0) break;
+	// }
 
-	//precisão máxima = 33
-	double euler = 0;
+	long double euler = 0.0L, fat = 1.0L;
 	for (int i = 0; i <= depth; i++) {
 		//famosa expansão de Taylor
-		double sum = (1.0/fatorial(i));
-		euler += sum;
-	};
-	printf("Número de Euler = %lf\n", euler);
+		if (i > 0) fat *= i;
+		euler += 1.0L / fat;
+	}
+	
+	printf("Número de Euler = %.16lf\n", (double)euler);
 
 	return 0;
 }
